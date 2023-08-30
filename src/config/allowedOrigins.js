b75808de2ef2f94801a744c85ab212a998e75924
Facundo Viale <91499618+GreyshadowARG@users.noreply.github.com>
2023-08-30 19:46:35 -0300

    cors config

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -2,7 +2,7 @@ const allowedOrigins = [
     '*',
     'http://localhost:3000',
     'http://localhost:8080',
-    'https://tranquil-pie-5a8ce2.netlify.app'
+    'https://app-bethel.netlify.app'
 ];
 
 
